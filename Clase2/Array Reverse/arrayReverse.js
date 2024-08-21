4815700f63b249e8840488e94b36f5a564943997
unknown <max.kmesana@gmail.com>
2024-08-21 15:14:20 -0300

    Finish Array Reverse exercises.

--- Clase2/Array Reverse/arrayReverse.js
+++ Clase2/Array Reverse/arrayReverse.js
@@ -11,7 +11,23 @@ function reverseArray(array){
     return responseArray;
 }
 
+function reverseArrayInPlace(array){
+    let arrayRightIndex = array.length - 1;
+    for(let i=0; i<=array.length; i++){
+        if(arrayRightIndex < i) break;
+        if(i === arrayRightIndex) break;
+        console.log(arrayRightIndex);
+
+        const leftElement = array[i];
+        array[i] = array[arrayRightIndex];
+        array[arrayRightIndex] = leftElement;
+        arrayRightIndex--;
+    }
+}
+
 let array = [1,2,3,4,5,6,7,8,9];
 //  
 // console.log(reverseArray(array));
 // console.log(array);
+reverseArrayInPlace(array);
+console.log(array);
