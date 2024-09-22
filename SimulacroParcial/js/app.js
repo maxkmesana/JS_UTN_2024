edf702dd4c3e8debee5d324058c33194151231fe
Max <max.kmesana@gmail.com>
2024-09-22 19:45:32 -0300

    Refactor todo.js for the last time, finish toDo.js, finish toDos.html, finished index.html.

--- SimulacroParcial/js/app.js
+++ SimulacroParcial/js/app.js
@@ -2,3 +2,55 @@
 Se espera que la logica este separada en distintos modulos
 */
 
+import { getUsers } from "./requests/userRequests.js";
+
+
+let selectedUser = undefined;
+const userTable = document.getElementById("user-table");
+const userHeader = document.getElementById("user-header");
+const todoBtn = document.getElementById("todos-btn");
+const todoAnchor = document.getElementById("todo-anchor");
+const postAnchor = document.getElementById("post-anchor");
+// LOAD
+document.addEventListener("DOMContentLoaded", async () => {
+    const data = await getUsers();
+    createTable(data);
+});
+
+function createTable(users){
+    for(const user of users){
+        createRow(user);
+    }
+}
+
+function createRow(user){
+    const newRow = document.createElement("tr");
+    newRow.id = user.id;
+    
+    const tdId = document.createElement("td");
+    tdId.textContent = user.id;
+
+    const tdUsername = document.createElement("td");
+    tdUsername.textContent = user.username;
+
+    const tdName = document.createElement("td");
+    tdName.textContent = user.name;
+
+    newRow.append(tdId, tdUsername, tdName);
+    userTable.appendChild(newRow);
+}
+
+// event on click of tr (set selected user id)
+userTable.addEventListener("click", async (event) => {
+    const clickedRow = event.target.closest("tr");
+    if(clickedRow){
+        selectedUser = await getUsers(clickedRow.id);
+
+        userHeader.textContent = selectedUser.username;
+        todoBtn.disabled = false;
+
+        todoAnchor.href += `?userId=${selectedUser.id}`;
+        // postAnchor.href += `?userId=${selectedUser.id}`;
+    }
+});
+
--- SimulacroParcial/js/toDo.js
+++ SimulacroParcial/js/toDo.js
@@ -1,63 +1,101 @@
-
-
 import { deleteTodo, getTodosByUserId, postToDo, putTodo } from "./requests/todoRequests.js";
 import { getUsers } from "./requests/userRequests.js";
 import { ToDo } from "./common/types.js";
-import { getSelectedUserId } from "./common/users.js";
-
 
 
 // GET
-const userList = document.getElementById("user-list");
-const toDoList = document.getElementById("todo-list");
+const todoTable = document.getElementById("table-body");
 const userHeader = document.getElementById("user-header");
+let selectedUser = undefined;
+const param = new URLSearchParams(window.location.search);
 
-// On click of user (li), get user's toDos and show them in list
-userList.addEventListener("click", (event) => {
-    toDoList.innerHTML = ""; // start anew
-    if(event.target.tagName === "LI"){
-
-        // get username from userId and update header
-        getUsers(event.target.id).then(data =>{
-            userHeader.textContent = data.username;
-        });
-
-        updateToDoList();
-    }
+document.addEventListener("DOMContentLoaded", async () => {
+    selectedUser = await getUsers(param.get("userId"));
+    userHeader.textContent = selectedUser.username;
+    updateTable();
 });
 
-// finds user's todos, renders them in LIs with delete buttons.
-function updateToDoList(){
-    toDoList.innerHTML = "";
-    let usersTodos = [];
-    getTodosByUserId(getSelectedUserId()).then((data) => { 
+// Contains check, delete and put
+function updateTable(){
+    todoTable.innerHTML = "";
+    getTodosByUserId(selectedUser.id).then((data) => {
         for(let toDoObject of data){
-            // prepare LI
-            const li = document.createElement("li");
-            li.id = toDoObject.id;
-            let liTextContent = `Title: ${toDoObject.title}`;
-            liTextContent += toDoObject.completed === true ? `, Completed: yes` : `, Completed: no`;
-            li.textContent =  liTextContent;    
+            const newRow = document.createElement("tr");
+            newRow.id = toDoObject.id;
+            
+            const tdId = document.createElement("td");
+            tdId.textContent = toDoObject.id;
 
-            // prepare delete button
-            const deleteBtn = document.createElement('button');
-            deleteBtn.innerText = 'X';
+            const tdTitle = document.createElement("td");
+            tdTitle.textContent = toDoObject.title;
+            tdTitle.contentEditable = true;
+
+            // EDIT FEATURE (PUT)
+            tdTitle.addEventListener("blur", async () => {
+                const newTitle = tdTitle.textContent.trim();
+                if(newTitle !== "" && newTitle !== toDoObject.title){
+                    await putTodo(toDoObject.id, {
+                        userId: toDoObject.userId,
+                        title: newTitle,
+                        completed: toDoObject.completed
+                    });
+                }
+            });
+
+            const tdCompleted = document.createElement("td");
+            tdCompleted.textContent = toDoObject.completed === true ? "yes" : "no";
 
             // DELETE
+            const deleteBtn = document.createElement("button");
+            deleteBtn.innerText = "X";
             deleteBtn.onclick = async function() {
                 this.parentElement.remove();
                 await deleteTodo(toDoObject.id);
-                updateToDoList();
+                updateTable();
             };
 
-            // append LI and delete button
-            li.appendChild(deleteBtn);
-            toDoList.appendChild(li);
+            // Check button (PUT)
+            const checkBtn = document.createElement("button");
+            checkBtn.innerText = "CHECK";
+            checkBtn.onclick = async function () {
+                checkToDo(newRow);
+            }
+
+            newRow.append(tdId, tdTitle, tdCompleted);
+            newRow.append(checkBtn, deleteBtn); 
+
+            todoTable.appendChild(newRow);
         }
     });
 }
 
-// POST
+async function checkToDo(row){
+    console.log("asdasd");
+    if(row){
+        const cells = row.cells;
+        
+        if(cells[2].textContent === "yes"){
+            cells[2].textContent = "no";
+
+            await putTodo(cells[0].textContent, {
+                userId: selectedUser.id,
+                title: cells[1].textContent,
+                completed: false
+            });
+        }else{
+            cells[2].textContent = "yes";
+
+            await putTodo(cells[0].textContent, {
+                userId: selectedUser.id,
+                title: cells[1].textContent,
+                completed: true
+            });
+        }
+    }
+}
+
+
+// // POST
 const submitBtn = document.getElementById("todo-submit-btn");
 const titleInput = document.getElementById("title-input");
 const radioTrue = document.getElementById("radio-true");
@@ -72,126 +110,6 @@ submitBtn.addEventListener("click", async (event) =>  {
         return;
     }
 
-    await postToDo(new ToDo(getSelectedUserId(), currentTitle, isCompleted));
-    updateToDoList();
+    await postToDo(new ToDo(selectedUser.id, currentTitle, isCompleted));
+    updateTable();
 });
-
-// PUT
-// let selectedLi = undefined;
-// toDoList.addEventListener("click", (event) => {
-//     if(event.target.tagName === 'LI'){
-
-//     }
-// })
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     getUsers().then(data => {
-//         for(let user of data){
-//             userList.push(user);
-//         }
-//         console.log("Initial users database populated successfully!");
-//     })
-//     .catch(error => console.log(`Could not populate user database: ${error}`));
-// });
-
-// const userListElement = document.getElementById("user-list");
-// const userHeader = document.getElementById("user-header");
-// const toDoListElement = document.getElementById("todo-list");
-// const submitToDo = document.getElementById("todo-submit-btn");
-// const title = document.getElementById("title-input");
-// const radioTrue = document.getElementById("radio-true");
-
-// let liToModify = undefined;
-// let titleToModify = undefined;
-// let radioToModify = undefined;
-
-// let selectedUserId = undefined;
-
-// GET
-// function populateSelectedTodoList(){
-//     toDoListElement.innerHTML = "";
-//     toDoList = [];
-
-//     getTodosByUserId(selectedUserId).then((data) => {renderToDoList()})
-//     .catch((error) => console.log(error));
-// }
-
-// function renderToDoList(){
-//     for(let toDoItem of toDoList){
-//         const li = document.createElement("li");
-//         li.id = toDoItem.id;
-
-//         let liTextContent = `Title: ${toDoItem.title}`;
-//         liTextContent += toDoItem.completed === true ? `, Completed: yes` : `, Completed: no`;
-//         li.textContent =  liTextContent;
-
-//         const deleteBtn = document.createElement('button');
-//         deleteBtn.innerText = 'X';
-//         deleteBtn.onclick = function() {
-//             this.parentElement.remove();
-//             deleteToDo(toDoItem.id);
-//         };
-//         li.appendChild(document.createTextNode('\u00A0\u00A0')); // Add some space
-//         li.appendChild(deleteBtn);
-
-//         toDoListElement.appendChild(li);
-//     }
-// }
-
-// POST/PUT
-// submitToDo.addEventListener("click",(event) => {
-//     event.preventDefault();
-    
-
-//     // POST
-//     const currentTitle = title.value;
-//     let isCompleted = radioTrue.checked === true ? true : false;
-
-//     if(currentTitle.trim() === ""){
-//         alert("Title can't be empty. Try again.");
-//         return;
-//     }
-//     postToDo(new ToDo(selectedUserId, currentTitle, isCompleted));
-//     title.value = "";
-//     populateSelectedTodoList();
-// });
-
-// userListElement.addEventListener("click", (event) => {
-//     if(event.target.tagName === 'LI'){
-//         selectedUserId = event.target.id;
-//         getUsers(selectedUserId).then(data => {userHeader.textContent = data.username});
-//         populateSelectedTodoList();
-//     }
-// });
-
-// function isValidToDo(currentTitle){
-//     if(currentTitle == null || currentTitle.trim() === ""){
-//         alert("Title can't be empty. Please try again.");
-//         return false;
-//     }
-// }
-
-// // DELETE
-// function deleteToDo(id){
-//     deleteTodo(id)
-//     .then(data => console.log(`Deleted: ${JSON.stringify(data)}`))
-//     .catch(error => console.log(error))
-// }
-
-// // PUT
-// toDoListElement.addEventListener("click", (event) => {
-//     if(event.target.tagName === 'LI'){
-//         if(liToModify !== undefined) liToModify.style.backgroundColor = null;
-//         liToModify = event.target;
-//         liToModify.style.backgroundColor = 'yellow';
-//     }
-// });
